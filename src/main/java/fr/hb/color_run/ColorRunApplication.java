09bc5620d9523244efefb984be71187f395bb2ec
Efikusu <35803896+Efikusu@users.noreply.github.com>
2025-01-16 09:06:11 +0100

    Add files via upload
    Ikimasho !

---
+++ src/main/java/fr/hb/color_run/ColorRunApplication.java
@@ -0,0 +1,13 @@
+package fr.hb.color_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/hb/color_run/ColorRunApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hb.color_run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
